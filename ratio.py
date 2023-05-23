9d88f60957ced0d1f38448c2c9ee67e9be19530c
Kaixin <kxuu0036@student.monash.edu>
2023-05-24 01:28:56 +1000

    First submit task2

--- ratio.py
+++ ratio.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import Generic, TypeVar
 from math import ceil
 from bst import BinarySearchTree
+from node import TreeNode
 
 T = TypeVar("T")
 I = TypeVar("I")
@@ -9,16 +10,41 @@ I = TypeVar("I")
 class Percentiles(Generic[T]):
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+        self.elements = BinarySearchTree()
     
     def add_point(self, item: T):
-        raise NotImplementedError()
+        self.elements[item] = item
     
     def remove_point(self, item: T):
-        raise NotImplementedError()
+        del self.elements[item]
 
     def ratio(self, x, y):
-        raise NotImplementedError()
+        total_elements = len(self.elements)
+        x_rank = int(ceil(total_elements * x / 100))
+        y_rank = int(ceil(total_elements * y / 100))
+
+        result = []
+        self._traverse_in_order(self.elements.root, x_rank, y_rank, result)
+        return result
+
+    def _traverse_in_order(self, current: TreeNode, x_rank: int, y_rank: int, result: List[T]) -> None:
+        if current is None or len(result) >= y_rank:
+            return
+
+        if current.left:
+            left_size = current.left.subtree_size
+        else:
+            left_size = 0
+
+        if left_size >= x_rank:
+            self._traverse_in_order(current.left, x_rank, y_rank, result)
+
+        if len(result) < y_rank:
+            result.append(current.item)
+
+        if left_size < y_rank:
+            self._traverse_in_order(current.right, x_rank - left_size - 1, y_rank - left_size - 1, result)
+
 
 if __name__ == "__main__":
     points = list(range(50))
