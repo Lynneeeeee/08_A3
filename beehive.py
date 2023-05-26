0e8a162f9d498fb668b9ecb09ee7629bdf65ae18
Kaixin <kxuu0036@student.monash.edu>
2023-05-27 03:25:59 +1000

    submit task6

--- beehive.py
+++ beehive.py
@@ -20,27 +20,42 @@ class BeehiveSelector:
         self.beehives = []
 
     def set_all_beehives(self, hive_list: list[Beehive]):
-        self.beehives = sorted(hive_list[:self.max_beehives], key=lambda hive: hive.volume, reverse=True)
-
+        self.beehives = sorted(hive_list[:self.max_beehives], key=lambda hive: min(hive.capacity, hive.volume) * hive.nutrient_factor, reverse=True)
 
     def add_beehive(self, hive: Beehive):
         if len(self.beehives) < self.max_beehives:
             self.beehives.append(hive)
-            self.beehives.sort(key=lambda hive: hive.volume, reverse=True)
+            self.beehives.sort(key=lambda hive: min(hive.capacity, hive.volume) * hive.nutrient_factor, reverse=True)
         else:
-            if hive.volume > self.beehives[-1].volume:
+            if min(hive.capacity, hive.volume) * hive.nutrient_factor > min(self.beehives[-1].capacity,
+                                                                            self.beehives[-1].volume) * self.beehives[
+                -1].nutrient_factor:
                 self.beehives.pop()
                 self.beehives.append(hive)
-                self.beehives.sort(key=lambda hive: hive.volume, reverse=True)
+                self.beehives.sort(key=lambda hive: min(hive.capacity, hive.volume) * hive.nutrient_factor,
+                                   reverse=True)
+
 
     def harvest_best_beehive(self):
         if len(self.beehives) > 0:
+            # Sort the beehives by potential emerald yield
+            self.beehives.sort(key=lambda hive: min(hive.capacity, hive.volume) * hive.nutrient_factor, reverse=True)
+
+            # Pop the first hive (with max potential yield)
             hive = self.beehives.pop(0)
+
+            # Calculate honey to harvest and update hive's volume
             honey_harvested = min(hive.capacity, hive.volume)
-            emeralds = honey_harvested * hive.nutrient_factor
             hive.volume -= honey_harvested
+
+            # If the hive still has honey, add it back to the list
             if hive.volume > 0:
-                self.beehives.insert(0, hive)
-            return emeralds
+                self.beehives.append(hive)
+
+            # Return the number of emeralds earned
+            return honey_harvested * hive.nutrient_factor
         else:
             return 0.0
+
+
+
