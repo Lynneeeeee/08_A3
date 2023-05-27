5a53e9aa1e9a4195d27b9aa5700b9112fb6ce20a
Kaixin <kxuu0036@student.monash.edu>
2023-05-28 04:24:47 +1000

    task5 add documentation

--- beehive.py
+++ beehive.py
@@ -40,20 +40,40 @@ class HeapBeehive:
 
 class BeehiveSelector:
     def __init__(self, max_beehives: int):
+        """
+            Initialize a BeehiveSelector with a maximum number of beehives that can be managed.
+            :param max_beehives: The maximum number of beehives that can be managed.
+            :complexity: O(1)
+        """
         self.max_beehives = max_beehives
         self.beehives_heap = MaxHeap(max_beehives)
 
     def set_all_beehives(self, hive_list: List[Beehive]):
+        """
+            Sets all the beehives in the manager with the given list of beehives.
+            :param hive_list: The list of Beehive objects to set.
+            :complexity: O(N), where N is the length of hive_list.
+        """
         self.beehives_heap = MaxHeap(self.max_beehives)
         for hive in hive_list:
             self.add_beehive(hive)
 
     def add_beehive(self, hive: Beehive):
+        """
+            Adds a new beehive to the manager, maintaining the `max_beehives` limit.
+            :param hive: The Beehive object to add.
+            :complexity: O(log N), where N is the number of beehives in the heap.
+        """
         hive_value = min(hive.capacity, hive.volume) * hive.nutrient_factor
         self.beehives_heap.add(HeapBeehive(beehive=hive, value=hive_value))
 
-
     def harvest_best_beehive(self):
+        """
+            Harvests honey from the beehive with the highest potential yield.
+            The beehive is removed from the heap if its honey volume becomes zero.
+            :return: The number of emeralds earned from the harvested honey.
+            :complexity: O(log N), where N is the number of beehives in the heap.
+        """
         if self.beehives_heap.length == 0:
             return None
 
@@ -67,6 +87,7 @@ class BeehiveSelector:
             self.add_beehive(best_beehive)
 
         return emeralds_earned
+
     # def __init__(self, max_beehives: int):
     #     """
     #         Initializes a maximum number of beehives.
