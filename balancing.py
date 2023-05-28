bfbeb6a355d07537f88c638fd5946ac5f14cfbe6
ylii0468 <ylii0468@student.monash.edu>
2023-05-28 17:46:49 +1000

    t4

--- balancing.py
+++ balancing.py
@@ -12,8 +12,7 @@ def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
     for x in x_coordinates:
         p.add_point(x)
 
-    percentile_values = p.ratio(0, 100)
-
+    percentile_values = p.ratio(0, 0)
     for value in percentile_values:
         for point in my_coordinate_list:
             if point[0] == value:
@@ -21,3 +20,17 @@ def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
                 break
 
     return results
+
+
+if __name__ == "__main__":
+    points = [
+        (1, 2, 3),
+        (4, 5, 6),
+        (7, 8, 9),
+        (2, 3, 1),
+        (5, 6, 4),
+        (8, 9, 7),
+    ]
+    new_ordering = make_ordering(points)
+    for point in new_ordering:
+        print(point)
